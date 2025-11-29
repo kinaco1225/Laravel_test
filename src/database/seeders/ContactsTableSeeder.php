cf5ed201767b7c018c089ce76bf8098ef6412119
yuya kishita <kishita.yyy@gmail.com>
2025-11-29 23:50:02 +0900

    修正

--- src/database/seeders/ContactsTableSeeder.php
+++ src/database/seeders/ContactsTableSeeder.php
@@ -14,6 +14,6 @@ class ContactsTableSeeder extends Seeder
      */
     public function run()
     {
-        COntact::factory()->count(3)->create();
+        COntact::factory()->count(10)->create();
     }
 } 
